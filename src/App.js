07035f142c74de7f8b70ccf5910a1e5c6a682165
Ravindran <ravindran.ramakannan@nutechnologyinc.com>
2020-12-26 11:54:10 +0530

    es6 class2

--- src/App.js
+++ src/App.js
@@ -10,12 +10,23 @@ class Colour{
     return this.clr;
   }
 }
-
+class Sky extends Colour{
+  constructor(clr,sky)
+  {
+    super(clr);
+  this.sky=sky;
+  }
+display()
+{
+  let variable=super.print();
+  return variable+this.sky;
+}
+}
 function App() {
   
-  const c=new Colour("Blue");
+  const c=new Sky("Blue"," which is sky colour");
     const [Toggle, value] = useState(false);
-    let displayContent = "Welcome Your favourite colour "+c.print();
+    let displayContent = "Welcome Your favourite colour "+c.display();
     const toggleHandler = () => {
       const presentState = Toggle;
       value(!presentState);
